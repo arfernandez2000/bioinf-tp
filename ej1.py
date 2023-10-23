e5fc6e4fa109e5e2312e2dc23a18c9b9afa5d92b
Natu17 <nlilienthal@itba.edu.ar>
2023-10-23 00:51:45 -0300

    ej1

--- ej1.py
+++ ej1.py
@@ -11,28 +11,58 @@
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+start_codons = ["ATG", "AUG"]
+stop_codons = ["TAA", "TAG", "TGA"]
+
+def search_orfs(sequence):
+    orfs = []
+
+    for start_codon in start_codons:
+        for stop_codon in stop_codons:
+            inside_orf = False
+            orf = ""
+            for i in range(0, len(sequence), 3):
+                codon = sequence[i:i+3]
+                if codon == start_codon:
+                    orf = codon
+                    inside_orf = True
+                elif codon == stop_codon and inside_orf:
+                    orf += codon
+                    orfs.append(orf)
+                    orf = ""
+                    inside_orf = False
+                elif inside_orf:
+                    orf += codon
+                elif orf != "" and not inside_orf:
+                    continue
+
+
+    return orfs
+def translate_and_save_sequences(input_file, output_file):
+
+
+    with open(input_file, "r") as in_handle, open(output_file, "w") as out_handle:
+        records = SeqIO.parse(in_handle, "genbank")
+
+        for record in records:
+            for frame in range(3):
+                nucleotide_sequence = record.seq[frame:]
+                nucleotide_sequence_rev = record.seq.reverse_complement()[frame:]
+
+                orfs = search_orfs(nucleotide_sequence)
+                orfs_rev = search_orfs(nucleotide_sequence_rev)
+
+                for orf in orfs + orfs_rev:
+                      if all(codon in start_codons for codon in (orf[:3])) and all(codon in stop_codons for codon in (orf[-3:])):
+                        amino_acid_sequence = orf.translate()
+                        fasta_record = SeqRecord(amino_acid_sequence, id=f"{record.id}_ORF",
+                                                    description=f"ORF from {record.id}",
+                                                    letter_annotations={"phred_quality": [40]*len(amino_acid_sequence)})
+                        SeqIO.write(fasta_record, out_handle, "fasta")
+
+    print(f"Secuencias traducidas y guardadas en '{output_file}'")
 
 input_file = "sequence.gb"
-
 output_file = "secuencias.fasta"
+translate_and_save_sequences(input_file, output_file)
 
-with open(input_file, "r") as in_handle, open(output_file, "w") as out_handle:
-    records = SeqIO.parse(in_handle, "genbank")
-
-    # Itera a través de las secuencias
-    for record in records:
-        for frame in range(1):
-            nucleotide_sequence = record.seq[frame:]
-            
-            print(nucleotide_sequence)
-        
-            amino_acid_sequence = nucleotide_sequence.translate()
-            
-            print(amino_acid_sequence)
-            fasta_record = SeqRecord(amino_acid_sequence, id=f"{record.id}_Frame{frame+1}", description=f"Translated from {record.id} Frame {frame+1}", letter_annotations={"phred_quality": [40]*len(amino_acid_sequence)})
-
-            print(fasta_record)
-            
-            SeqIO.write(fasta_record, out_handle, "fasta")
-
-print(f"Secuencias traducidas y guardadas en '{output_file}'")
