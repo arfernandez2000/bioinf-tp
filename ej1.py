da294d59b82125d0a2129398f27cfa6c603be3ab
Natu17 <nlilienthal@itba.edu.ar>
2023-11-14 11:10:40 -0300

    add translate to aminoacids sequence in ej1

--- ej1.py
+++ ej1.py
@@ -4,10 +4,19 @@ from Bio.SeqRecord import SeqRecord
 import argparse
 import os
 
+
 parser = argparse.ArgumentParser(description='Ejercicio 1. Nucleotide GenBank -> Protein FASTA')
 parser.add_argument('-i', help='Input GenBank file (default = inputs/sequence.gb)', default='inputs/sequence.gb')
-parser.add_argument('-o', help='Output Fatsa file (default = results/secuencias.fas)', default='results/secuencias.fasta')
+parser.add_argument('-o', help='Output Fatsa file (default = results/secuencias_a.fas)', default='results/secuencias_a.fasta')
 args = parser.parse_args()
+
+def translate_and_save(orfs, record, out_handle):
+    for orf in orfs:
+        amino_acid_sequence = Seq(orf).translate(cds=True)
+        fasta_record = SeqRecord(amino_acid_sequence, id=f"{record.id}_ORF",
+                                description=f"ORF from {record.id}",
+                                letter_annotations={"phred_quality": [40]*len(amino_acid_sequence)})
+        SeqIO.write(fasta_record, out_handle, "fasta")
 
 def save_orfs_to_file(record, orfs, output_directory):
     # Create a directory for the record if it doesn't exist
@@ -57,7 +66,7 @@ def translate_and_save_sequences(input_file, output_file):
             sequence = sequence[::-1] 
             for i in range(3):
                 orfs.extend(search_orfs(sequence[i:]))
-        
+            translate_and_save(orfs,record,out_handle)
             save_orfs_to_file(record,orfs,"results")
     print(f"Secuencias traducidas y guardadas en '{output_file}'")
 
