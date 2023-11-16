31e72fcbade670ee910655ab97f117e6f3942b85
Martín E. Zahnd <mzahnd@itba.edu.ar>
2023-11-16 16:03:22 -0300

    Fix dirname is a file

--- dir_helper.py
+++ dir_helper.py
@@ -5,7 +5,7 @@ import os
 
 def create_output_dirs_from_file_path(output_file: str) -> None:
     dir = os.path.dirname(output_file)
-    if os.path.exists(dir):
+    if not dir or os.path.exists(dir):
         return
 
     os.makedirs(dir)
